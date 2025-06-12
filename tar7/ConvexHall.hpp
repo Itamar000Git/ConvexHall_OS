9f7304e0db86351b01ab6f680c8a24289f6c5760
Itamar000Git <itamarbabai98@gmail.com>
2025-06-12 16:25:00 +0300

    commit8

---
+++ tar7/ConvexHall.hpp
@@ -0,0 +1,27 @@
+#ifndef CONVEXHALL_HPP
+#define CONVEXHALL_HPP
+#include <vector>
+typedef struct Point{
+    double x;
+    double y;
+
+    bool operator<(const Point& other) const {
+        return x < other.x || (x == other.x && y < other.y);
+    }
+} Point;
+
+typedef struct ConvexHull{
+    std::vector<Point> points;
+    int size;
+    double area;
+} ConvexHull;
+void printConvexHull(const ConvexHull& hull, std::ostream& os = std::cout);
+void convexHull(std::vector<Point> points, ConvexHull& hull);
+double cross(const Point& p1, const Point& p2, const Point& p3);
+double polygonArea(const std::vector<Point>& poly);
+void readPoints(ConvexHull& graph, int n,std::istringstream& iss);
+void addPoint(ConvexHull& graph, std::istringstream& iss);
+void removePoint(ConvexHull& graph, std::istringstream& iss);
+void handle_request(const std::string& request, int client_socket);
+void wakeup_server();
+#endif
